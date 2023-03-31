9bfbbedd8277b0239b43fe292a406bfc64b4fa5e
Ibraim <87862261+zibroo@users.noreply.github.com>
2023-03-31 15:34:40 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1b"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
 
